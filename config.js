eccdbf7ae03474b5d94b2dc0729bca2eeaca655f
nurbolattashbolat02 <nurbolat.tashbolat@gmail.com>
2026-09-22 22:33:57 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-/* ------------------------------------------------------------------
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVydWdrcGF4ZmxjZXNsaWZlYmFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk0OTAyMTEsImV4cCI6MjEwNTA2NjIxMX0.AkQRjFAJII3fYBCLQsGWYzBoshW-RA4cQJtvBYMUCas/* ------------------------------------------------------------------
    Настройки сайта. Меняется только этот файл.
 
    1. url  — адрес проекта Supabase (Project Settings → Data API → Project URL)
